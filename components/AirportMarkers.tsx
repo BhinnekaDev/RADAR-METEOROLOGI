cdc523d9a7bc04fbf2b4a438b2ff41f33a8c59ac
fifovalle <fifanaufal10@gmail.com>
2025-07-17 13:40:23 +0700

    fix: memperbaiki agar bandara jadi logo bmkg

--- components/AirportMarkers.tsx
+++ components/AirportMarkers.tsx
@@ -8,22 +8,10 @@ const msToKnots = (ms: number) => {
 };
 
 const airportIcon = divIcon({
-    html: `
-    <div class="airport-icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 64 64">
-        <circle cx="32" cy="32" r="30" fill="white" stroke="black" stroke-width="2"/>
-        <path d="M2 32 Q16 28 32 32 Q48 36 62 32" stroke="#1E90FF" stroke-width="4" fill="none"/>
-        <path d="M2 24 Q16 20 32 24 Q48 28 62 24" stroke="#1E90FF" stroke-width="4" fill="none"/>
-        <path d="M2 40 Q16 36 32 40 Q48 44 62 40" stroke="#1E90FF" stroke-width="4" fill="none"/>
-        <path d="M0 46 H64" stroke="green" stroke-width="4"/>
-        <path d="M0 50 H64" stroke="green" stroke-width="4"/>
-        <path d="M0 54 H64" stroke="green" stroke-width="4"/>
-        </svg>
-    </div>
-  `,
-    className: "",
-    iconSize: [28, 28],
-    iconAnchor: [14, 14],
+    html: `<img src="/icon.png" alt="Airport Icon" style="width: 45px; height: 45px;" />`,
+    className: "custom-airport-icon",
+    iconSize: [24, 24],
+    iconAnchor: [12, 12],
 });
 
 interface AirportMarkersProps {
